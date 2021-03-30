63029f7b4fa730c7beac27b58e8e04a3c1a14022
gjones444 <jkerr1870@gmail.com>
2021-03-29 20:10:03 -0400

    setup chat engine

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import './App.css';
 const App = () => {
     return (
         <ChatEngine 
-            height="100vh"
-            projectID="14545fe2-47e3-4405-ae28-0753bb8133c8" 
-            userName="Test"
-            userSecret="1234"
+        height=""
+        projectID="" 
+        userName=""
+        userSecret=""
         />
     )
 };
