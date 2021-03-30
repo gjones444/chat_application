d8b3436edc80af91b85201098e7f580b86b5b075
gjones444 <jkerr1870@gmail.com>
2021-03-29 20:53:30 -0400

    Template created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { ChatEngine } from 'react-chat-engine';
-
 import './App.css';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const App = () => {
     return (
-        <ChatEngine 
-        height=""
-        projectID="" 
-        userName=""
-        userSecret=""
-        />
+        <ChatEngine
+            height=""
+            projectID="" 
+            userName=""
+            userSecret=""                  
+        />  
     )
+    
 };
 
 export default App;
